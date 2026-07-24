36bf100dca2b9be7e704062e5e350224a61b5313
SavjeetSingh <savjeetworking@gmail.com>
2026-07-24 12:26:53 +0530

    feat: add report filter query and CSV export utilities

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom/vitest'
+
+// Mock URL.createObjectURL for tests
+if (typeof global.URL.createObjectURL === 'undefined') {
+  global.URL.createObjectURL = () => 'blob:mock-url'
+  global.URL.revokeObjectURL = () => {}
+}
